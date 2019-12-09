75a291c9d11a8265af4f6b485c7d2e9179d771a7
ZHXLL <1061136599@qq.com>
2019-12-09 16:13:49 +0800

    呵呵哒

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -11,58 +11,58 @@ import img4 from '../images/~ADADON4VZH(G@LAGP57)YX.png'
 import img5 from '../images/recruitLandlord_banner.jpg'
 
 
-var image=[img1,img2,img3,img4,img5]
+var image = [img1, img2, img3, img4, img5]
 
 
 class Carousel extends React.Component {
-	 constructor(props){
-    super(props);
-    this.state={
-    	index:0,
-    	itm:setInterval(()=>{
-    		this.setState({
-    			index:this.state.index+1
-    		})
-    		if(this.state.index>=5){
-	    		this.setState({
-	    			index:0 
-	    		})
-	    	}
-    	},2000)
-    }
-  }
-	 btn(res){
-  	this.setState({
-  		index:res
-  	})
-  }
- componentWillUnmount(){
-   clearInterval(this.state.itm)
-}
-  render() {
-    return (
-     	<div className='lbbox'>
-     		{
-     			image.map((item,index)=>{
-     				return (
-     					<div key={index} src={item} className='img' style={{backgroundImage:'url('+ item +')',opacity:this.state.index==index?1:0}}></div>
-     				)
-     			})
-     		}
-     		<div className='btnbox'>
-     				<div className='btnb'>
-	     				{
-	     					image.map((item,index)=>{
-	     						return (
-	     							<div key={index} className={this.state.index==index?"btn a1":'btn'} onClick={()=>{this.btn(index)}}></div>
-	     						)
-	     					})
-	     				}
-     				</div>     				
-     		</div>
-     	</div>
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			index: 0,
+			itm: setInterval(() => {
+				this.setState({
+					index: this.state.index + 1
+				})
+				if (this.state.index >= 5) {
+					this.setState({
+						index: 0
+					})
+				}
+			}, 2000)
+		}
+	}
+	btn(res) {
+		this.setState({
+			index: res
+		})
+	}
+	componentWillUnmount() {
+		clearInterval(this.state.itm)
+	}
+	render() {
+		return (
+			<div className='lbbox'>
+				{
+					image.map((item, index) => {
+						return (
+							<div key={index} src={item} className='img' style={{ backgroundImage: 'url(' + item + ')', opacity: this.state.index == index ? 1 : 0 }}></div>
+						)
+					})
+				}
+				<div className='btnbox'>
+					<div className='btnb'>
+						{
+							image.map((item, index) => {
+								return (
+									<div key={index} className={this.state.index == index ? "btn a1" : 'btn'} onClick={() => { this.btn(index) }}></div>
+								)
+							})
+						}
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 Carousel.defaultProps = {
